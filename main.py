9101d08f319174b9e0f0286247d12e9da25e7e6e
Lenon Maguire <lenon.maguire@gmail.com>
2017-02-08 21:46:43 -0600

    trying to get this to work. All error messages are showing up

--- main.py
+++ main.py
@@ -1,86 +1,157 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import cgi
+import re
 
-# # html boilerplate for the top of every page
-# page_header =
-# """<!DOCTYPE!>
-# <html>
-# <head>
-#     <title>User Signup</title>
-#     <style type="text/css">
-#         .error {
-#         color: red;
-#         }
-#     <style>
-# </head>
-# <body>
-#     <h1>
-#         <a href="/">Signup</a>
-#     </h1>
-# """"
-#
-# # html boilerplate for the bottom of every page
-# page_footer = """
-# </body>
-# </html>
-# """
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+        .inputs {
+
+            text-align: left;
+        }
+    </style>
+</head>
+
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</html>
+"""
 
 
-def build_page(textarea_content):
-    user_label = "<label> Username:</label>"
-    user_input = "<input type='text' name='user'/>"
+# a form for adding new username
+form = """
+<body>
+    <h1>Signup</h1>
+    <form method="post">
+       <label>Username
+            <input type="text" name="username" value="%(username)s"/>
+            <span class="error"> %(username_error)s</span>
+        </label>
+<br>
+        <label>Password
+            <input type="password" name="password" value="%(password)s"/>
+            <span class="error">%(password_error)s</span>
+        </label>
+<br>
+        <label>Verify Password
+            <input type="password" name="verify_password" value="%(verify_password)s"/>
+            <span class="error">%(verify_error)s</span>
+        </label>
+<br>
+        <label>Email (optional)
+            <input type="text" name="email" value="%(email)s"/>
+            <span class="error"> %(email_error)s</span>
+        </label>
+<br>
+        <input type="submit" value="Submit"/>
+    </form>
+</body>
+"""
 
-    password_label = "<label>Password:</label>"
-    password_input = "<input type='text' name='password' />"
+class SignUpHandler(webapp2.RequestHandler):
+    def write_form(self, username="", username_error="", password="", password_error="",
+                   verify_password="", verify_error="", email="", email_error=""):
+        values = {"username": username,
+                  "username_error": username_error,
+                  "password": password,
+                  "password_error": password_error,
+                  "verify_password": verify_password,
+                  "verify_error": verify_error,
+                  "email": email,
+                  "email_error": email_error}
 
-    verify_label = "<label>Verify Password</label>"
-    verify_input  = "<input type='text' name='verify' />"
+        response = page_header + form % values + page_footer
+        self.response.write(response)
 
-    email_label = "<label>Email (optional)</lanel>"
-    email_verify = "<input type='text' name='email' />"
+    #validation functions
+    def validate_username(self, username):
+        USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        return USER_RE.match(username)
 
-    submit = "<input type='submit' />"
+    def validate_password(self, password):
+        USER = re.compile(r"^.{3,20}$")
+        return USER.match(password)
 
-    header = "<h2>Signup</h2>"
+    def validate_verify(self,verify_password):
+        VERY = re.compile(r"^.{3,20}$")
+        return VERY.match(verify_password)
 
-    form = ("<form method='post'>" +
-            user_label + user_input + "<br>" +
-            password_label + password_input + "<br>" +
-            verify_label + verify_input + "<br>" +
-            email_label + email_verify + "<br>" +
-            submit + "</form>")
+    def validate_email(self, email):
+        Email_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
+        return Email_RE.match(email)
 
-    return header + form
 
-# class Index(webapp2.RequestHandler):
-#     """ Handles requests coming in to '/' (the root of our site)
-#         e.g. www.flicklist.com/
-#     """
-#
-#     def get(self):
-#
 
-class MainHandler(webapp2.RequestHandler):
+
+
     def get(self):
-        content = build_page("")
-        self.response.write(content)
+        self.write_form()
+
+    def post(self):
+
+        have_error = False
+
+        #username validation
+        username = self.request.get("username")
+        valid_username = self.validate_username(username)
+
+        #password validation
+        password = self.request.get("password")
+        valid_password = self.validate_password(password)
+
+        #verify validation
+        verify_password = self.request.get("verify_password")
+        valid_verify = self.validate_password(verify_password)
+
+        #email validation
+        email = self.request.get("email")
+        valid_email = self.validate_email("email")
+
+        if not valid_username:
+            username_error = "That's not a valid username"
+            have_error = True
+            self.write_form(username=username, username_error=username_error)
+            if not valid_password:
+                password_error = "That's not a valid password"
+                have_error = True
+                self.write_form(password=password, password_error=password_error)
+                if not valid_verify:
+                    verify_error = "Those passwords do not match!"
+                    have_error = True
+                    self.write_form(verify_password=verify_password, verify_error=verify_error)
+                    if not valid_email:
+                        email_error = "That's not a real email address"
+                        have_error = True
+        if have_error == True:
+            self.write_form(username=username, username_error=username_error,
+                            password=password, password_error=password_error,
+                            verify_password=verify_password, verify_error=verify_error,
+                            email=email, email_error=email_error)
+
+        # self.redirect('/welcome?username=%s' % password)
+            # self.write_form(email=email, email_error=email_error)
+        else:
+            self.redirect('/welcome?username=%s' % username)
+
+
+
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        self.response.write('<h1>Welcome, %s!' % username)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', SignUpHandler),
+    ('/welcome', WelcomeHandler)
 ], debug=True)
+
